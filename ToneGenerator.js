1686045979bb4949a7968c054e3fe9e74d52c338
Vincent Toups <vincent.toups@gmail.com>
2017-03-26 14:21:00 -0400

    Created a tone generator.

---
+++ ToneGenerator.js
@@ -0,0 +1,26 @@
+(function(global){
+
+    // Lets use the default C major scale (two octaves).
+    var defaultScale = 'C2 D2 E2 F2 G2 A2 B2 C3 D3 E3 F3 G3 A3 B3'.split(' ');
+
+    function ToneGenerator(initial,step,scale){
+        scale = typeof scale === "undefined" ? defaultScale : scale;
+        step = typeof step === "undefined" ? 1 : step;
+        this.state = initial;
+        this.step = step;
+        this.scale = scale;
+    };
+
+    ToneGenerator.prototype.generateTones = function(n){
+        var notes = [];
+        while(n>0){
+            notes.push(this.scale[this.state%this.scale.length]);
+            this.state = this.state + this.step;
+            n--;
+        }
+        return notes;
+    };
+
+    window.ToneGenerator = ToneGenerator;
+    
+})(window);
