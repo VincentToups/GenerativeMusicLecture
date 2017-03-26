f0413bc2d1583405600431ef019b2d516b12c48a
Vincent Toups <vincent.toups@gmail.com>
2017-03-26 12:47:55 -0400

    Created project skeleton.

---
+++ README.md
@@ -0,0 +1,7 @@
+Generative Music In the Browser
+===============================
+
+This is a repository containing a tutorial generative art piece for
+the Iron Yard.
+
+
---
+++ index.js
@@ -0,0 +1,6 @@
+document.addEventListener("DOMContentLoaded",main);
+
+function main(){
+    //We need to generate some sounds to play
+    //We need to play some sound
+}
